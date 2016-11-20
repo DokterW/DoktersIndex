38ba8cd7ad67ae8bd22f648dec29c0397c0a395a
Dr. Waldijk <DokterW@users.noreply.github.com>
2016-11-20 10:24:09 +0100

    chk

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Dokter's Index
+
+This is a very simple and crude file manager that started out as a test. I needed a simple file manager for my [git](https://en.wikipedia.org/wiki/Git) bash shell script, [LazyGit](https://github.com/DokterW/LazyGit).
+
+All you can do is to move up and down in your folders.
+
+If I find the time I might add some extra features later.
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Dokter's Index v0.1
+# Dokter's Index v0.5
 # Made by Dr. Waldijk
 # Dokter's own file manager.
 # Read the README.md for more info.
@@ -11,38 +11,52 @@
 #    DOKINDLIAS=$(pwd)
 #    echo "alias doktersindex='$DOKINDLIAS/start.sh'" >> ~/.bashrc
 #fi
-# Comfig ----------------------------------------------------------------------------
-DOKINDVER="0.1"
+# Config ----------------------------------------------------------------------------
+DOKINDVER="0.5"
 DOKINDNAM="Dokter's Index"
 # -----------------------------------------------------------------------------------
 while :
 do
     clear
     DOKINDDIR=$(pwd)
+    DOKINDLS=$(ls -1)
     echo "$DOKINDNAM v$DOKINDVER"
     echo ""
-    ls -1
-    echo "Current directory: $DOKINDDIR"
+    echo "$DOKINDLS" | nl -nrz -w2 -s- | sed 's/-/ /g'
+    echo "[$DOKINDDIR]"
     echo ""
-    echo "1. Move down one folder"
-    echo "2. Move up one folder (you need to enter it manually)"
-    echo "Q. Quit"
+    echo "   00. Move down one folder"
+    echo "01-99. Move up one folder"
+    echo "   QQ. Quit"
     echo ""
-    read -p "Enter option: " -s -n1 DOKIND
+    read -p "Enter option: " -s -n2 DOKIND
     case "$DOKIND" in
-        1)
+        00)
             cd ..
         ;;
-        2)
-            clear
-            echo "$DOKINDNAM v$DOKINDVER"
-            echo ""
-            ls -1
-            echo ""
-            read -p "Enter directory: " DOKINDDIR
-            cd $DOKINDDIR
+        [0-9]*)
+            DOKINDCNT=$(echo "$DOKINDLS" | wc -l)
+            if [ "$DOKIND" -ge "$DOKINDCNT" ]
+            then
+                clear
+                echo "No folder with that number, Butterfingers!"
+                sleep 3s
+            else
+                if [ "$DOKIND" = 0[1-9] ]
+                then
+                    DOKIND=$(echo $DOKIND | cut -c 2)
+                fi
+                DOKINDDIR=$(echo "$DOKINDLS" | sed "$DOKIND!d")
+#                clear
+#                echo "$DOKINDNAM v$DOKINDVER"
+#                echo ""
+#                ls -1
+#                echo ""
+#                read -p "Enter directory: " DOKINDDIR
+                cd $DOKINDDIR
+            fi
         ;;
-        [qQ])
+        qq|QQ)
             clear
             break
         ;;
